13406fb3d5cee1d026327d70e5f0328fbf1935a5
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-07-15 17:05:48 -0300

    Initial commit of scrapping players from futbin instead of fifacm

--- scrapper/src/main.py
+++ scrapper/src/main.py
@@ -11,10 +11,10 @@ import io
 import json
 import os
 
-positions_dict = {'Goalkeepers': 'GK', 'Center Backs': 'CB', 'Full Backs': 'RB,LB', 'Defensive Midfielders': 'CDM,CM',
+positions_dict = {'Goalkeepers': 'GK', 'Center Backs': 'CB', 'Full Backs': 'RB,LB, RWB, LWB', 'Defensive Midfielders': 'CDM,CM',
                  'Ofensive Midfielders': 'CAM', 'Wingers': 'LW,LF,LM,RF,RW,RM', 'Attackers': 'ST,CF'}
 
-base_url = "https://www.fifacm.com/players?position="
+base_url = "https://www.futbin.com/players?page=1&version=gold_rare&pos_type=all&position="
 
 def save_image(img, directory, file_name):
     image_content = requests.get(img).content
@@ -55,123 +55,140 @@ for index, name in enumerate(positions_dict):
         driver.maximize_window()
         time.sleep(5)
 
-        igs_btns = driver.find_elements(By.CLASS_NAME, "igs-btn")
-        scroll = 0
+        players_1 = driver.find_elements(By.CLASS_NAME, "player_tr_1")
+        players_2 = driver.find_elements(By.CLASS_NAME, "player_tr_2")
+        players = players_1 + players_2
         cont = 0
-        scrollStride = 450
-        if positions_dict[name] == 'GK':
-            scrollStride = 210
-
-        for btn in igs_btns:
-            btn.click()
-            driver.execute_script("window.scrollTo({}, {})".format(
-                scroll, scroll + scrollStride))
-            scroll += scrollStride
-            time.sleep(1)
-
-            element = driver.find_elements(
-                By.CLASS_NAME, "site-players-page")[0]
-            htmlContent = element.get_attribute('outerHTML')
-
-            page_soup = soup(htmlContent, "html.parser")
-
-            table = page_soup.find("table")
-            trList = table.findAll("tr")[1+cont:1+cont+2]
-
-            # Basic Info
+        
+        for p in players:
             player = {
                 'position': positions[-1]['id']
             }
 
-            tds = trList[0].findAll("td")
+            tds = p.findAll("td")
+
             player['id'] = tds[0].find(
                 'div', {"class": "igs-btn"})['data-playerid']
-
-            try:
-                other_player = next(filter(lambda x: x.id == player['id'], players))
-                cont += 2
-                continue
-            except:
-                pass
-
-            team = (tds[1].findAll(
-                'img', {"class": "team-img"})[0]['data-original-title']).strip()
-            nation = (tds[1].findAll(
-                'img', {"class": "team-img"})[1]['data-original-title']).strip()
-            team_img = tds[1].findAll(
-                'img', {"class": "team-img"})[0]['src']
-            nation_img = tds[1].findAll(
-                'img', {"class": "team-img"})[1]['src']
             
-            try:
-                team_obj = next(filter(lambda x: x['name'] == team, teams))
-            except StopIteration:
-                if('notfound' not in team_img):
-                    file_path = save_image(team_img, 'teams', f"{team.replace(' ', '')}.jpg")
-                    team_img = file_path
-                    
-                teams.append({
-                    'id': len(teams) + 1,
-                    'name': team,
-                    'image_path': team_img
-                })
-                
-                team_obj = teams[-1]
-                
-            player['team_origin'] = team_obj['id']
-                
-            try:
-                nation_obj = next(filter(lambda x: x['name'] == nation, nations))
-            except StopIteration:
-                if('notfound' not in nation_img):
-                    file_path = save_image(nation_img, 'nations', f"{nation.replace(' ', '')}.jpg")
-                    nation_img = file_path
-                    
-                nations.append({
-                    'id': len(nations) + 1,
-                    'name': nation,
-                    'image_path': nation_img
-                })
-                
-                nation_obj = nations[-1]
-                
-            player['nation'] = nation_obj['id']
-
-            player['specific_position'] = ''.join(tds[1].find(
-                'div', {"class": "player-position-cln"}).findAll(text=True)).split(',')[0].split('|')[0].strip()
-            stats = trList[1].find(
-                "div", {"class": "player-stats"}).findAll("div", {"class": "col-md-2"})
-            player['name'] = (''.join(tds[1].find(
-                'div', {"class": "player-name"}).find("a").findAll(text=True))).strip()
-
+            player['name'] = tds[1].find(
+                'a', {"class": "player_name_players_table"}).text
+            
+            players_club_nation = tds[1].find(
+                'span', {"class": "players_club_nation"}).findAll("a")
+            player['team'] = players_club_nation[0]['data-original-title']
+            player['nation'] = players_club_nation[1]['data-original-title']
+            player['league'] = players_club_nation[2]['data-original-title']
+            
             player_img_url = tds[1].find(
-                'img', {"class": "player-img-info"})['src']
-            
+            'img', {"class": "player_img"})['src']
+        
             if('notfound_player' not in player_img_url):
                 file_path = save_image(player_img_url, 'players', f"{player['name'].replace(' ', '')}_{player['id']}.jpg")
                 player['image_path'] = file_path
             else:
                 cont += 2
                 continue
+
+
+        # page_soup = soup(htmlContent, "html.parser")
+
+        # table = page_soup.find("table")
+        # trList = table.findAll("tr")[1+cont:1+cont+2]
+
+        # # Basic Info
+        # player = {
+        #     'position': positions[-1]['id']
+        # }
+
+        # tds = trList[0].findAll("td")
+        # player['id'] = tds[0].find(
+        #     'div', {"class": "igs-btn"})['data-playerid']
+
+        # try:
+        #     other_player = next(filter(lambda x: x.id == player['id'], players))
+        #     cont += 2
+        #     continue
+        # except:
+        #     pass
+
+        # team = (tds[1].findAll(
+        #     'img', {"class": "team-img"})[0]['data-original-title']).strip()
+        # nation = (tds[1].findAll(
+        #     'img', {"class": "team-img"})[1]['data-original-title']).strip()
+        # team_img = tds[1].findAll(
+        #     'img', {"class": "team-img"})[0]['src']
+        # nation_img = tds[1].findAll(
+        #     'img', {"class": "team-img"})[1]['src']
+        
+        # try:
+        #     team_obj = next(filter(lambda x: x['name'] == team, teams))
+        # except StopIteration:
+        #     if('notfound' not in team_img):
+        #         file_path = save_image(team_img, 'teams', f"{team.replace(' ', '')}.jpg")
+        #         team_img = file_path
                 
-            player['overall'] = ''.join(tds[2].find(
-                'div', {"class": "player-overall"}).findAll(text=True))
-            player['pace'] = ''.join(stats[0].find(
-                "div", {"class": "main-stat-rating-title"}).findAll(text=True))
-            player['shooting'] = ''.join(stats[1].find(
-                "div", {"class": "main-stat-rating-title"}).findAll(text=True))
-            player['passing'] = ''.join(stats[2].find(
-                "div", {"class": "main-stat-rating-title"}).findAll(text=True))
-            player['dribbling'] = ''.join(stats[3].find(
-                "div", {"class": "main-stat-rating-title"}).findAll(text=True))
-            player['defending'] = ''.join(stats[4].find(
-                "div", {"class": "main-stat-rating-title"}).findAll(text=True))
-            player['physical'] = ''.join(stats[5].find(
-                "div", {"class": "main-stat-rating-title"}).findAll(text=True))
+        #     teams.append({
+        #         'id': len(teams) + 1,
+        #         'name': team,
+        #         'image_path': team_img
+        #     })
+            
+        #     team_obj = teams[-1]
+            
+        # player['team_origin'] = team_obj['id']
+            
+        # try:
+        #     nation_obj = next(filter(lambda x: x['name'] == nation, nations))
+        # except StopIteration:
+        #     if('notfound' not in nation_img):
+        #         file_path = save_image(nation_img, 'nations', f"{nation.replace(' ', '')}.jpg")
+        #         nation_img = file_path
+                
+        #     nations.append({
+        #         'id': len(nations) + 1,
+        #         'name': nation,
+        #         'image_path': nation_img
+        #     })
+            
+        #     nation_obj = nations[-1]
+            
+        # player['nation'] = nation_obj['id']
 
-            players.append(player)
+        # player['specific_position'] = ''.join(tds[1].find(
+        #     'div', {"class": "player-position-cln"}).findAll(text=True)).split(',')[0].split('|')[0].strip()
+        # stats = trList[1].find(
+        #     "div", {"class": "player-stats"}).findAll("div", {"class": "col-md-2"})
+        # player['name'] = (''.join(tds[1].find(
+        #     'div', {"class": "player-name"}).find("a").findAll(text=True))).strip()
 
-            cont += 2
+        # player_img_url = tds[1].find(
+        #     'img', {"class": "player-img-info"})['src']
+        
+        # if('notfound_player' not in player_img_url):
+        #     file_path = save_image(player_img_url, 'players', f"{player['name'].replace(' ', '')}_{player['id']}.jpg")
+        #     player['image_path'] = file_path
+        # else:
+        #     cont += 2
+        #     continue
+            
+        # player['overall'] = ''.join(tds[2].find(
+        #     'div', {"class": "player-overall"}).findAll(text=True))
+        # player['pace'] = ''.join(stats[0].find(
+        #     "div", {"class": "main-stat-rating-title"}).findAll(text=True))
+        # player['shooting'] = ''.join(stats[1].find(
+        #     "div", {"class": "main-stat-rating-title"}).findAll(text=True))
+        # player['passing'] = ''.join(stats[2].find(
+        #     "div", {"class": "main-stat-rating-title"}).findAll(text=True))
+        # player['dribbling'] = ''.join(stats[3].find(
+        #     "div", {"class": "main-stat-rating-title"}).findAll(text=True))
+        # player['defending'] = ''.join(stats[4].find(
+        #     "div", {"class": "main-stat-rating-title"}).findAll(text=True))
+        # player['physical'] = ''.join(stats[5].find(
+        #     "div", {"class": "main-stat-rating-title"}).findAll(text=True))
+
+        # players.append(player)
+
+        # cont += 2
 
 driver.quit()
 
