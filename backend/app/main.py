da48221d430a2fa1b3951bcf68f9334162c6f21d
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-08-16 12:18:06 -0300

    Updating register of routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,11 @@
 
 from fastapi import FastAPI
 
-from app.routers import player
+from app.routers import participant, player, position, team
 
 app = FastAPI()
 
-app.include_router(player.router, prefix="/players", tags=["users"])
+app.include_router(player.router, prefix="/player", tags=["player"])
+app.include_router(position.router, prefix="/position", tags=["position"])
+app.include_router(team.router, prefix="/team", tags=["team"])
+app.include_router(participant.router, prefix="/participant", tags=["participant"])
